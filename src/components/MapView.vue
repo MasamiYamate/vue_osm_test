25e958124effbc179a393e276be080d2ff833c93
MasamiYamate <MasamiYamate@MasamiYamate-MacBookAir.local>
2019-01-10 10:28:25 +0900

    initialize commit

---
+++ src/components/MapView.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id='OSMCanvas'>
+    </div>
+</template>
+<script>
+import 'ol/ol.css'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TitleLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+
+export default {
+    name: 'mapview',
+    components: {},
+    data: function () {
+        return {
+            mapview: null
+        }
+    },
+    methods: {
+        createMap () {
+            this.mapview = new Map ({
+                target: 'OSMCanvas',
+                layers: [
+                    new TitleLayer({
+                        source: new OSM()
+                    })
+                ],
+                view: new View({
+                    center:[0,0],
+                    zoom:0
+                })
+            })
+        }
+    },
+    mounted() {
+        this.createMap()
+    }
+}
+
+</script>
+
+<style scoped>
+
+#OSMCanvas {
+    width: 100%;
+    height: 100%;
+}
+
+</style>
